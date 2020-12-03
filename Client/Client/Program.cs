3697b471574d05eccd07c21aeb89c17c2c0696d7
Buket PEHLİVAN <buketpeh@gmail.com>
2020-12-03 09:56:40 +0300

    .
    .

--- Client/Client/Program.cs
+++ Client/Client/Program.cs
@@ -12,13 +12,18 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);// Soket oluşturulmaktadır.
-            s.Connect(IPAddress.Parse("192.168.1.185"),600);//Aynı Ip adresi ve porta bağlamaktayız.192.168.1.185 127.0.0.1
-            byte[] buffer = Encoding.ASCII.GetBytes(Console.ReadLine());//Ekrana girilecek değer dönüşümü yapılmaktadır.Stream kullanıldığı için veriler byte şeklinde gönderilmektedir.
-            s.Send(buffer);
-            s.Close();
+            Program b = new Program();
+            b.DataInput();
             Console.Read();
 
         }
+        public void DataInput(){
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);// Soket oluşturulmaktadır.
+            s.Connect(IPAddress.Parse("192.168.1.185"), 600);//Aynı Ip adresi ve porta bağlamaktayız.192.168.1.185 127.0.0.1
+            byte[] buffer = Encoding.ASCII.GetBytes(Console.ReadLine());//Ekrana girilecek değer dönüşümü yapılmaktadır.Stream kullanıldığı için veriler byte şeklinde gönderilmektedir.
+            s.Send(buffer);
+            s.Close();
+        }
+
     }
 }
